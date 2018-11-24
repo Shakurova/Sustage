c8b0aabd51bd0fe64d9b77391fce1ec4774ef73a
maobedkova <maryobedkova@gmail.com>
2018-11-24 19:24:54 +0200

    add badge enlight

--- badges.py
+++ badges.py
@@ -18,6 +18,7 @@ class Person:
             else:
                 if behaviour in ["meat_free", "packaged_food"]:
                     self.__dict__[behaviour].bvalue_decrement()
+            self.__dict__[behaviour].enlight_badge()
 
 
 class Purchase:
@@ -37,10 +38,11 @@ class Purchase:
 
 
 class Badges:
-    def __init__(self, descr, treshold=10):
+    def __init__(self, descr, treshold=3):
         self.description = descr
         self.treshold = treshold
         self.value = 0
+        self.usable = False
 
     def bvalue_increment(self):
         """We found the badge behaviour in the receipt."""
@@ -49,12 +51,16 @@ class Badges:
     def bvalue_decrement(self):
         self.value = 0
 
+    def enlight_badge(self):
+        if self.value == self.treshold:
+            self.usable = True
+
 
 def test(person):
     for name in person.__dict__:
         badge = person.__dict__[name]
         try:
-            print(badge.description, badge.value)
+            print(badge.description, badge.value, badge.usable)
         except:
             print(name, badge)
 
