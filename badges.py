c94f201747c5126c38b022fe5b92be93c88e03aa
maobedkova <maryobedkova@gmail.com>
2018-11-24 18:43:28 +0200

    mookup badges implementation

---
+++ badges.py
@@ -0,0 +1,82 @@
+
+class Person:
+    def __init__(self):
+        # self.sustage_score = 0
+        self.meat_free = Badges('meat_free')
+        self.alchohol = Badges('alcohol')
+        self.fair_trade = Badges('fair_trade')
+        self.buy_local = Badges('local')
+        self.packaged_food = Badges('packaged_food')
+        # to be continued
+
+    def badges_update(self, purchase_summary):
+        """If a person gets the badge"""
+        for behaviour in purchase_summary.__dict__:
+            if purchase_summary.__dict__[behaviour]:
+                # conditionally more complex later
+                self.__dict__[behaviour].bvalue_increment()
+            else:
+                if behaviour in ["meat_free", "packaged_food"]:
+                    self.__dict__[behaviour].bvalue_decrement()
+
+
+class Purchase:
+    def __init__(self, mf=False, al=False, ft=False, bl=False, pf=False):#, receipt_data):
+        """If purchase was sustainable"""
+        self.meat_free = mf
+        self.alchohol = al
+        self.fair_trade = ft
+        self.buy_local = bl
+        self.packaged_food = pf
+
+        # to be continued
+        # self.check_purchase(receipt_data)
+
+    # def check_purchase(self, receipt_data):
+        # filling in every attribute
+
+
+class Badges:
+    def __init__(self, descr, treshold=10):
+        self.description = descr
+        self.treshold = treshold
+        self.value = 0
+
+    def bvalue_increment(self):
+        """We found the badge behaviour in the receipt."""
+        self.value += 1
+
+    def bvalue_decrement(self):
+        self.value = 0
+
+
+def test(person):
+    for name in person.__dict__:
+        badge = person.__dict__[name]
+        try:
+            print(badge.description, badge.value)
+        except:
+            print(name, badge)
+
+
+if __name__ == "__main__":
+    # we have data for one person for one purchase
+    person = Person()
+    test(person)
+    purchase = Purchase(True, True, False, True, False) #Purchase(receipt_data)
+    person.badges_update(purchase)
+    print("="*20)
+    test(person)
+    purchase = Purchase(True, True, True, True, True)
+    person.badges_update(purchase)
+    print("="*20)
+    test(person)
+    purchase = Purchase(True, False, True, False, False)
+    person.badges_update(purchase)
+    print("="*20)
+    test(person)
+
+
+
+
+
